d8837c7f572a9172fa0ae5f4f73c1f477d13e9ca
zhaoqiaochu <zhaoqiaochu@rinna.co.jp>
2022-01-20 11:44:01 +0800

    project init

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const YourComponent = () => (
+  <Image
+    src='/images/profile.jpg' // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt='Your Name'
+  />
+)
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href='/'>
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <YourComponent />
+    </>
+  )
+}
